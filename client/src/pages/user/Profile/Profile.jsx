9c9e5441de6e0f4798b7263702108d867aed6964
Krishnanunni.S <krishnanunni2424@gmail.com>
2023-11-18 08:06:59 +0530

    profile import updated

--- client/src/pages/user/Profile/Profile.jsx
+++ client/src/pages/user/Profile/Profile.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ProfileContent from '../../../components/users/profile/profileContent'
+import ProfileContent from '../../../components/users/profile/ProfileContent'
 import SideNav from '../../../components/sideNav/SideNav'
 import PageWrapper from '../../../components/admin/PageWrapper'
 import NavBar from '../../../components/users/navBar/NavBar'
